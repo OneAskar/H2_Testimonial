eddcf7f7a02d07b4aa33e39bacee6681d2475141
kh-almas <services.almas@gmail.com>
2026-09-06 23:23:15 +0600

    add model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -28,5 +30,38 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'user_roles')
+            ->withPivot('assigned_by_user_id')
+            ->withTimestamps();
+    }
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(UserProfile::class);
+    }
+
+    public function practitioner(): HasOne
+    {
+        return $this->hasOne(Practitioner::class);
+    }
+
+    public function consents(): HasMany
+    {
+        return $this->hasMany(UserConsent::class);
+    }
+
+    public function testimonials(): HasMany
+    {
+        return $this->hasMany(Testimonial::class, 'author_user_id');
+    }
+
+    public function marketingSubscriptions(): HasMany
+    {
+        return $this->hasMany(MarketingSubscription::class);
     }
 }
